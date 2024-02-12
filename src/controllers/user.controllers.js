969e6c97e2493c191a1a20cadc3b63fd946bba77
Armaan <yadavarmaan10@gmail.com>
2024-02-12 08:55:02 +0530

    add logout functionality

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -186,6 +186,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
+  //add logout functinality => refer to previous commit
   await User.findByIdAndUpdate(
     req.user._id,
     {
